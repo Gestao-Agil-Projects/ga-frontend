2f687032bed2d5796316d3cf76bb9a60dc423f69
Gustavo Müller <gustavomuller@MacBook-Air-de-Gustavo-3.local>
2025-09-15 23:58:24 -0300

    feat: config reactotron

---
+++ ReactotronConfig.ts
@@ -0,0 +1,6 @@
+import Reactotron from "reactotron-react-js";
+
+Reactotron.configure({}) // controls connection & communication settings
+	.connect(); // let's connect!
+
+export default Reactotron;
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import { Routes } from "./routes";
 
+// Importar Reactotron apenas em desenvolvimento
+if (import.meta.env.DEV) {
+  import("../ReactotronConfig");
+}
+
 function App() {
   return <Routes />;
 }
