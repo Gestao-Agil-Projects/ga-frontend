52cfba3701bd906ecbe9f5aa22c7dbf7aff39b7a
Gustavo Müller <gustavomuller@MacBook-Air-de-Gustavo-3.local>
2025-09-22 23:35:59 -0300

    feat: new screen

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import { Home } from "../screens/Home";
 import { About } from "../screens/About";
 import { Contact } from "../screens/Contact";
+import { Professionals } from "../screens/Professionals";
 
 export function AppRoutes() {
   return (
@@ -9,6 +10,7 @@ export function AppRoutes() {
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
       <Route path="/contact" element={<Contact />} />
+      <Route path="/professionals" element={<Professionals />} />
     </Routes>
   );
 }
--- src/routes/types.ts
+++ src/routes/types.ts
@@ -6,6 +6,7 @@ export interface AppRouteParams {
   home: undefined;
   about: undefined;
   contact: undefined;
+  professionals: undefined;
 }
 
 export type AppRouteNames = keyof AppRouteParams;
