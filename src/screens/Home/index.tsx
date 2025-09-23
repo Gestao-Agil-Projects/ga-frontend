1a57bd5d8b469603d92cc729418b6480bb2609b2
Gustavo Müller <gustavomuller@MacBook-Air-de-Gustavo-3.local>
2025-09-22 23:35:26 -0300

    feat: remove divs tests

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -10,23 +10,6 @@ export function Home() {
             Home
           </h1>
         </div>
-        <div className="flex flex-row gap-4">
-          <div className="p-4 w-36 h-36 bg-success rounded-lg flex items-center justify-center">
-            <h2 className=" text-2xl font-bold text-warning-70">
-              Teste bg-success
-            </h2> 
-          </div>
-          <div className="p-4 w-36 h-36 bg-error rounded-lg flex items-center justify-center">
-            <h2 className="text-2xl font-bold text-black">
-              Teste bg-error
-            </h2> 
-          </div>
-          <div className="p-4 w-36 h-36 bg-warning-70 rounded-lg flex items-center justify-center">
-            <h2 className=" text-2xl font-bold text-black">
-              Teste bg-warning-70
-            </h2> 
-          </div>
-        </div>
       </main>
     </div>
   );
